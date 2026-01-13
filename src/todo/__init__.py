635da94611fed1cd4b1636a04f8dea1f2dfe1c49
Aman Iqbal <amaaniqbal0011@gmail.com>
2026-01-14 00:11:37 +0500

    phase 2

--- src/todo/__init__.py
+++
@@ -1,6 +0,0 @@
-from .entities import Task
-from .manager import TaskManager
-from .persistence import Persistence
-from .cli import CLIInterface
-
-__all__ = ["Task", "TaskManager", "Persistence", "CLIInterface"]
